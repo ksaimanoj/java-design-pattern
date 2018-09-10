2e9a1939a7f0074edd933d2677b548b730f245e5
ksaimanoj <saimanoj.kadiyala@gmail.com>
2018-09-10 14:09:29 +0530

    Changed AdapterImpl to Adapter
    We can use the objects at the interface level

--- AdapterPattern/src/Main.java
+++ AdapterPattern/src/Main.java
@@ -2,9 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		AdapterImpl adapterImpl = new AdapterImpl();
+		Adapter adapter = new AdapterImpl();
 		
-		adapterImpl.request();
+		adapter.request();
 	}
 
 }
